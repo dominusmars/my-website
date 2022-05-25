ae018ffab2d87beef219d26c40416586b46cae34
Mohamad Khawam <55813440+dominusmars@users.noreply.github.com>
2022-05-25 19:33:30 -0400

    checks for files that start with . in write ups

--- pages/api/writeups.js
+++ pages/api/writeups.js
@@ -5,6 +5,9 @@ export default async function handler(req, res) {
     var info = resedInfo.data
     for (let index = 0; index < info.length; index++) {
         const element = info[index];
+        if (element.name.startsWith(".")) {
+            continue;
+        }
         var data = await axios.get(element.download_url)
         result.push({
             'name': element.name,
